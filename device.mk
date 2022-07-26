e51def6ce7885ec81c4496e21b235ef283e72e64
TheMalachite <eliasgheeraert@gmail.com>
2022-07-27 01:26:52 +0200

    selene: Drop MediaTek thermal services
    * mi_thermald do the job already

--- device.mk
+++ device.mk
@@ -432,12 +432,6 @@ PRODUCT_PACKAGES += \
     libshim_sensors \
     libpiex_shim
 
-# Thermal
-PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@2.0 \
-    android.hardware.thermal@2.0.vendor
-
 # TinyXML
 PRODUCT_PACKAGES += \
     libtinyxml
