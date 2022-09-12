51ab0cd5bc9d00abd7e6aefa7dffa22edf03f4d7
bengris32 <bengris32@protonmail.ch>
2022-09-12 20:17:03 +0100

    selene: Build MediaTek Vibrator HAL
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Signed-off-by: atharvnegi <atharvnegi2951@gmail.com>

--- device.mk
+++ device.mk
@@ -370,8 +370,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl \
-    android.hardware.vibrator@1.0-service
+    android.hardware.vibrator-service.mediatek
 
 # Shims
 PRODUCT_PACKAGES += \
