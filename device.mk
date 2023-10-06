80000e0a708256244f730f65333fedf785071174
bengris32 <bengris32@protonmail.ch>
2023-10-06 10:14:12 +0200

    selene: Switch to AOSP Boot implementation
    * We can no longer use the MediaTek boot control implementation
      due to legal issues. Switch to the AOSP one.
    
    Change-Id: I1d983fd7148ca40a00c3cf21b6773d2c5ecb78f4
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -17,9 +17,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
 # A/B
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.1-service \
-    android.hardware.boot@1.1-mtkimpl \
-    android.hardware.boot@1.1-mtkimpl.recovery
+    android.hardware.boot@1.2-impl \
+    android.hardware.boot@1.2-impl.recovery \
+    android.hardware.boot@1.2-service
 
 PRODUCT_PACKAGES += \
     update_engine \
