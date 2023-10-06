ec4f1b98d97345cac0b5a646c80be812d15b61dd
Sarthak Roy <sarthakroy2002@gmail.com>
2023-10-06 04:10:16 +0000

    selene: Build `android.frameworks.sensorservice@1.0.vendor`
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Change-Id: Iff9eaf0b79476e2b86aa21154f8b4030bbe1d16b

--- device.mk
+++ device.mk
@@ -382,7 +382,8 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0.vendor \
     android.hardware.sensors@2.0.vendor \
     android.hardware.sensors@2.0-ScopedWakelock.vendor \
-    android.frameworks.sensorservice@1.0
+    android.frameworks.sensorservice@1.0 \
+    android.frameworks.sensorservice@1.0.vendor
 
 PRODUCT_PACKAGES += \
     libsensorndkbridge
