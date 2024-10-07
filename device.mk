423f819a040cffc74d0766829ed9ce1465bd6698
atharvnegi <atharvnegi2951@gmail.com>
2024-10-07 15:14:13 +0530

    Revert "selene: dt2w: Drop DT2W service"
    This reverts commit 99879f273a9f446d7dd14ea69c0e154e56ddbc28.

--- device.mk
+++ device.mk
@@ -217,6 +217,13 @@ PRODUCT_PACKAGES += \
     libssl.vendor \
     libui.vendor
 
+# DT2W
+PRODUCT_PACKAGES += \
+    DT2W-Service-MT6768
+
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/dt2w/dt2w_event:$(TARGET_COPY_OUT_VENDOR)/bin/dt2w_event
+
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-service \
