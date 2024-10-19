f5fa5d33b2ad7f85ccc7d62200dbb9ee9bfc3d6c
clarencelol <clarencekuiek@proton.me>
2024-10-19 08:50:17 +0530

    selene: Update PRODUCT_BUILD_PROP_OVERRIDES format
    =* Ref https://review.lineageos.org/c/LineageOS/android_build_soong/+/401215
    
    Signed-off-by: clarencelol <clarencekuiek@proton.me>
    Signed-off-by: atharvnegi <atharvnegi2951@gmail.com>

--- lineage_selene.mk
+++ lineage_selene.mk
@@ -44,7 +44,7 @@ PRODUCT_MANUFACTURER := xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_VENDOR := xiaomi
 TARGET_VENDOR_PRODUCT_NAME := selene
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="selene-user 13 TP1A.220624.014 V14.0.6.0.TKUMIXM release-key"
+PRODUCT_BUILD_PROP_OVERRIDES += BuildDesc=$(call normalize-path-list, "selene-user 13 TP1A.220624.014 V14.0.6.0.TKUMIXM release-key")
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := "Redmi/selene/selene:12/TP1A.220624.014/V14.0.6.0.TKUMIXM:user/release-keys"
